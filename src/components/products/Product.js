68d1bfe4aa3f25f7f7a5462e60aa31057cf16355
Teresa Tuesday <teresa.beredo@gmail.com>
2018-03-15 14:05:46 -0600

    Added UpdateProduct

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react'
 import Paper from 'material-ui/Paper'
-
+import { ModalButton } from '../buttons'
+import UpdateProduct from '../forms/UpdateProduct'
 
 export default class extends Component {
   render(){
     const {product} = this.props
     return(
       <Paper className='product'>
+        <h2>{product.name}</h2>
         <img src={product.imgURL} alt={'Not Available'} width="20%" height="20%"/>
-        <section>
-          <h2>{product.name}</h2>
-          <div>{product.price}</div>
-          <p>{product.desc}</p>
-        </section>
+        <div>{product.price}</div>
+        <p>{product.desc}</p>
+        <ModalButton label="Update" display={<UpdateProduct product={product} />} />
       </Paper>
     )
   }
