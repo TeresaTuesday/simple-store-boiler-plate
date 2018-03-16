7c8242624bc2467019df42ac86d3130da53bbce3
Teresa Tuesday <teresa.beredo@gmail.com>
2018-03-16 09:54:18 -0600

    Added Background Image

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import ProductsList from '../pages/ProductsList'
+import Login from '../components/forms/Login'
 
 // const Home = () => (
 //   <div>Home Page</div>
 // )
 
+
+
 export default () => (
   <Switch>
     <Route exact path="/" component={ProductsList} />
+    <Route path="/Login" component={Login} />
+
   </Switch>
 )
--- src/index.js
+++ src/index.js
@@ -2,26 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import './styles/main.css'
+import './images/FireworksHome.jpg'
+
+//import nav from './components/nav'
 
 import {apollo} from './config/apollo'
 import Routes from './config/routes'
 
 const App = (
   <div>
-    {/*<Nav/>*/}
+     {/*<nav/>*/}
     <Routes/>
   </div>
 )
+
+
 const AppWrapper = (
-  
-  <ApolloProvider client={apollo}>
+  <div className="mainPage">
+    <ApolloProvider client={apollo}>
     <MuiThemeProvider>
+      <AppBar className='header'
+              title='Firework Gettin Place'
+              iconClassNameRight='muidocs-icon-navigation-expand-more'/>
+      
       <Router>
         {App}
       </Router>
     </MuiThemeProvider>
   </ApolloProvider>
+    </div>
 )
 
 ReactDOM.render(
